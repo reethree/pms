16b376e06139ecd58097ae8a1432310ca5a8b2a4
Reza R Ramadhan <reza@contrivent.com>
2019-02-04 14:53:33 +0700

    Update

--- resources/views/modules/order/email.blade.php
+++ resources/views/modules/order/email.blade.php
@@ -15,6 +15,122 @@
         <div class="content">                
 
             <div class="message">
+                
+                @foreach($order_product as $o_prod)
+                    <div class="header" style="text-align: center;">
+                        <img src="{{ asset("image/Logo.png")}}" style="width: 200px;" />
+                        <hr  color='#f6f6f6' style="margin: 30px;" />
+                        <h3>DATA SPESIFIKASI PRODUK</h3>
+                        <h2>{{$o_prod->name}}</h2>
+                        <img src="{{ asset("uploads/product/".$o_prod->photo)}}" width="250" />
+                        <hr color='#f6f6f6' style="margin: 30px;" />
+                    </div>
+                    <div class="desc-produk">
+                        <div class="column">
+                            <table style="border: 1px solid #CCC;margin-bottom: 5px;">
+                                <tr>
+                                    <td style="padding: 15px;text-align: center;border-right: 1px solid #CCC;width: 50%;">Nama Customer</td>
+                                    <td style="padding: 15px;text-align: center;">
+                                        <p><b>{{$o_prod->customer_name}}</b></p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        @if(count($o_prod->materials) > 0)
+                            <div class="column">
+                                <table style="border: 1px solid #CCC;margin-bottom: 5px;">
+                                    <tr>
+                                        <td style="padding: 15px;text-align: center;border-right: 1px solid #CCC;width: 50%;">Material Produk</td>
+                                        <td style="padding: 15px;text-align: center;">
+                                            @foreach($o_prod->materials as $material)
+                                                <p><b>{{$material->material_name}} - {{$material->qty.' Kg'}}</b></p>
+                                            @endforeach  
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        @endif
+                        
+                        <div class="column">
+                            <table style="border: 1px solid #CCC;margin-bottom: 5px;">
+                                <tr>
+                                    <td style="padding: 15px;text-align: center;border-right: 1px solid #CCC;width: 50%;">Berat</td>
+                                    <td style="padding: 15px;text-align: center;">
+                                        <p><b>Prediksi : {{$o_prod->weight_pre.' Gram'}}</b></p>
+                                        <p><b>Buffer : {{$o_prod->weight_buff.' Gram'}}</b></p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="column">
+                            <table style="border: 1px solid #CCC;margin-bottom: 5px;">
+                                <tr>
+                                    <td style="padding: 15px;text-align: center;border-right: 1px solid #CCC;width: 50%;">Estimasi Order</td>
+                                    <td style="padding: 15px;text-align: center;">
+                                        <p><b>{{number_format($o_prod->quantity).' pcs / Month'}}</b></p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="column">
+                            <table style="border: 1px solid #CCC;margin-bottom: 5px;">
+                                <tr>
+                                    <td style="padding: 15px;text-align: center;border-right: 1px solid #CCC;width: 50%;">Perkiraan CT</td>
+                                    <td style="padding: 15px;text-align: center;">
+                                        <p><b>{{$o_prod->cycle_time.' Detik'}}</b></p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        @if(count($o_prod->machines) > 0)
+                            <div class="column">
+                                <table style="border: 1px solid #CCC;margin-bottom: 5px;">
+                                    <tr>
+                                        <td style="padding: 15px;text-align: center;border-right: 1px solid #CCC;width: 50%;">Mesin Yang Digunakan</td>
+                                        <td style="padding: 15px;text-align: center;">
+                                            @foreach($o_prod->machines as $machine)
+                                                <p><b>{{$machine->machine_name}}</b></p>
+                                            @endforeach
+                                            
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        @endif
+                        <div class="column">
+                            <table style="border: 1px solid #CCC;margin-bottom: 5px;">
+                                <tr>
+                                    <td style="padding: 15px;text-align: center;border-right: 1px solid #CCC;width: 50%;">Jumlah Cavity</td>
+                                    <td style="padding: 15px;text-align: center;">
+                                        <p><b>{{$o_prod->cavity.' Cavity'}}</b></p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        @if(count($o_prod->moulds) > 0)
+                            <div class="column">
+                                <table style="border: 1px solid #CCC;margin-bottom: 5px;">
+                                    <tr>
+                                        <td style="padding: 15px;text-align: center;border-right: 1px solid #CCC;width: 50%;">Mould Yang Digunakan</td>
+                                        <td style="padding: 15px;text-align: center;">
+                                            @foreach($o_prod->moulds as $mould)
+                                                <p><b>{{$mould->mould_name}}</b></p>
+                                            @endforeach
+                                            
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        @endif 
+                    </div>
+
+                @endforeach
+                <br /><br />
+                <hr />
+                <br />
+                <div class="header" style="text-align: center;">
+                    <h3>PERHITUNGAN HARGA PRODUK</h3>
+                </div>
                 <!-- Table row -->
                 <div class="row">
                   <div class="col-xs-12 table-responsive">
@@ -22,7 +138,7 @@
                       <thead>
                       <tr>
                         <th>#</th>
-                        <th>PRODUCT INFO</th>
+                        <th>PRODUCT NAME</th>
                         <th>MONTHLY<br />QUANTITY</th>
                         <th>MATERIAL<br />(Cost)</th>
                         <th>MOULD<br />(Cost)</th>
@@ -37,21 +153,7 @@
                             @foreach($order_product as $o_prod)
                                 <tr>
                                     <td style="text-align: center;">{{$i}}</td>
-                                    <td>
-                                        <b>{{'Customer : '.$o_prod->customer_name}}</b><br /><br />
-                                        <img src="{{ asset("uploads/product/".$o_prod->photo)}}" width="120" /><br />
-<!--                                        {{'Product Name : '.$o_prod->name}}<br />
-                                        {{'Weight Prediction : '.$o_prod->weight_pre.' Gram'}}<br />
-                                        {{'Weight Buffer : '.$o_prod->weight_buff.' Gram'}}<br />
-                                        {{'Efficiency Actual : '.$o_prod->efficiency_actual.'%'}}<br />
-                                        {{'Efficiency Buffer : '.$o_prod->efficiency_buffer.'%'}}-->
-                                        Name : {{$o_prod->name}}<br />
-                                        Weight (Buffer): {{$o_prod->weight_buff}}<br />
-                                        Material Efficiency : {{$o_prod->efficiency_buffer.'%'}}<br /><br />
-                                        Cavity : {{$o_prod->cavity}}<br />
-                                        Cycle Time : {{$o_prod->cycle_time}}<br />
-                                        Time Efficiency : {{$o_prod->efficiency.'%'}}<br />
-                                    </td>
+                                    <td>{{$o_prod->name}}</td>
                                     <td style="vertical-align: middle;text-align: center;">{{number_format($o_prod->quantity)}}</td>
                                     <td style="vertical-align: middle;text-align: center;"><b>{{$o_prod->material_cost}}</b></td>  
                                     <td style="vertical-align: middle;text-align: center;"><b>{{$o_prod->mould_cost}}</b></td> 
@@ -67,15 +169,15 @@
                                 ?>
                                 @if(count($o_prod->materials) > 0)
                                     <tr style="background: aquamarine;">
-                                        <th colspan="2">Material Name</th>
-                                        <th colspan="3">Quantity</th>
+                                        <th colspan="3">Material Name</th>
+                                        <th colspan="2">Quantity</th>
                                         <th colspan="2">Amount (Cost)</th>
                                         <th colspan="2">Amount (Buffer)</th>
                                     </tr>
                                     @foreach($o_prod->materials as $material)
                                     <tr>
-                                        <td colspan="2">{{$material->material_name}}</td>
-                                        <td colspan="3" style="text-align: center;">{{$material->qty.' Kg'}}</td>
+                                        <td colspan="3">{{$material->material_name}}</td>
+                                        <td colspan="2" style="text-align: center;">{{$material->qty.' Kg'}}</td>
                                         <td colspan="2" style="text-align: right;">{{number_format($material->cost)}}</td>
                                         <td colspan="2" style="text-align: right;">{{number_format($material->price)}}</td>
                                     </tr>
@@ -84,8 +186,8 @@
                                 
                                 @if(count($o_prod->moulds) > 0)
                                     <tr style="background: aquamarine;">
-                                        <th colspan="2">Mould Name</th>
-                                        <th>Cavity</th>
+                                        <th colspan="3">Mould Name</th>
+                                        <!--<th>Cavity</th>-->
                                         <th colspan="2">Depreciation</th>
                                         
                                         @if(\Auth::user()->role == 'owner')
@@ -95,8 +197,8 @@
                                     </tr>
                                     @foreach($o_prod->moulds as $mould)
                                     <tr>
-                                        <td colspan="2">{{$mould->mould_name}}</td>
-                                        <td style="text-align: center;">{{$mould->cavity}}</td>
+                                        <td colspan="3">{{$mould->mould_name}}</td>
+                                        <!--<td style="text-align: center;">{{$mould->cavity}}</td>-->
                                         <td colspan="2" style="text-align: center;">{{$mould->mould_depr.' Month'}}</td>
                                         <td colspan="2" style="text-align: right;">{{number_format($mould->mould_cost)}}</td>
                                         <td colspan="2" style="text-align: right;">{{number_format($mould->mould_buff)}}</td>
@@ -106,16 +208,16 @@
                                 
                                 @if(count($o_prod->machines) > 0)
                                     <tr style="background: aquamarine;">
-                                        <th colspan="2">Machine Name</th>
-                                        <th>Cycle Time</th>
+                                        <th colspan="3">Machine Name</th>
+                                        <!--<th>Cycle Time</th>-->
                                         <th colspan="2">Depreciation</th>
                                         <th colspan="2">Amount (Cost)</th>
                                         <th colspan="2">Amount (Buffer)</th>
                                     </tr>
                                     @foreach($o_prod->machines as $machine)
                                     <tr>
-                                        <td colspan="2">{{$machine->machine_name}}</td>   
-                                        <td style="text-align: center;">{{$machine->cycle_time.' Sec'}}</td>
+                                        <td colspan="3">{{$machine->machine_name}}</td>   
+                                        <!--<td style="text-align: center;">{{$machine->cycle_time.' Sec'}}</td>-->
                                         <td colspan="2" style="text-align: center;">{{$machine->depreciation.' Year'}}</td>
                                         <td colspan="2" style="text-align: right;">{{number_format($machine->depr_amount)}}</td>
                                         <td colspan="2" style="text-align: right;">{{number_format($machine->amount)}}</td>
@@ -231,7 +333,7 @@
                         <tr>
                             <td colspan="2">&nbsp;</td>
                         </tr>
-                        <tr style="font-size: 18px;">
+                        <tr style="font-size: 16px;">
                           <td>Total COST per PCS:</td>
                           <td style="text-align: right;"><b>{{number_format(($p_total_cost+$total_cost+$o_over->amount),2)}}</b></td>
                         </tr>
@@ -274,7 +376,7 @@
                         <tr>
                             <td colspan="2">&nbsp;</td>
                         </tr>
-                        <tr style="font-size: 18px;">
+                        <tr style="font-size: 16px;">
                           <td>Selling Price per PCS:</td>
                           <td style="text-align: right;"><b>{{number_format(($p_total_price+$total_price+$o_over->amount+$o_over->profit),2)}}</b></td>
                         </tr>
